2a3fac78135d5796242da10efc087a0ab39b211f
aymen2018 <aymenmosrati@gmail.com>
2024-02-16 15:04:29 +0100

    update code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB connection
-mongoose.connect('mongodb://root:example@mongodb:27017/mydatabase', {
+mongoose.connect('mongodb://root:example@localhost:27017/mydatabase', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   authSource: 'admin', // MongoDB authentication source
@@ -76,5 +76,5 @@ app.listen(PORT, '0.0.0.0', () => {
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, How are you version');
+  res.send('Hello, How are you version-test');
 });
